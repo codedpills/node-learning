37e80e44c781d720ed39797adbe6c9c638e40790
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-11-28 13:57:10 +0000

    old index file

--- test.js
+++ test.js
@@ -41,3 +41,100 @@ server.use(handleRequest);
 server.listen(3000, '127.0.0.1', () => {
 	console.log('server is running');
 });
+
+const readFileAsArray = function (file, cb = () => {}) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, function (err, data) {
+			if (err) {
+				reject(err);
+				return cb(err);
+			}
+			const lines = data.toString().trim().split('\n');
+			resolve(lines);
+			cb(null, lines);
+		});
+	});
+};
+
+async function countOdd() {
+	try {
+		const lines = await readFileAsArray('./numbers');
+		const numbers = lines.map(Number);
+		const oddCount = numbers.filter((n) => n % 2 === 1).length;
+		console.log('Odd numbers count:', oddCount);
+	} catch (err) {
+		console.error(err);
+	}
+}
+countOdd();
+
+//From docs
+// server.get(
+// 	'/user/:id',
+// 	function (req, res, next) {
+// 		console.log('ID:', req.params.id);
+// 		next();
+// 	},
+// 	function (req, res, next) {
+// 		res.send('User Info');
+// 	}
+// );
+
+// // handler for the /user/:id path, which prints the user ID
+// server.get('/user/:id', function (req, res, next) {
+// 	res.end(req.params.id);
+// });
+
+// server.get(
+// 	'/user/:id',
+// 	function (req, res, next) {
+// 		// if the user ID is 0, skip to the next route
+// 		if (req.params.id === '0') next('route');
+// 		// otherwise pass the control to the next middleware function in this stack
+// 		else next();
+// 	},
+// 	function (req, res, next) {
+// 		// send a regular response
+// 		res.send('regular');
+// 	}
+// );
+
+// // handler for the /user/:id path, which sends a special response
+// server.get('/user/:id', function (req, res, next) {
+// 	res.send('special');
+// });
+
+//Loading ROUTER
+// const router = express.Router();
+
+// router.use((req, res, next) => {
+// 	res.send('From Router function');
+// 	next();
+// });
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+// router.use(
+// 	'/user/:id',
+// 	function (req, res, next) {
+// 		console.log('Request URL:', req.originalUrl);
+// 		next();
+// 	},
+// 	function (req, res, next) {
+// 		console.log('Request Type:', req.method);
+// 		next();
+// 	}
+// );
+
+// router.use(
+// 	'/user/:id',
+// 	(req, res, next) => {
+// 		res.send('jgkhbn');
+// 		console.log('Request Type:', req.originalUrl);
+// 		next();
+// 	},
+// 	(req, res, next) => {
+// 		console.log(req.method);
+// 		next();
+// 	}
+// );
+// server.use('/', router);
